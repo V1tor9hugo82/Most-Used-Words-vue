d4dc01bcfc73c5024c5f81d5a29e526db0a72a19
vitor hugo <vhrocha17@gmail.com>
2022-01-30 17:09:24 -0300

    primeira parte do meu progeto de traduçao

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <toolbar-title>Most User Words</toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+export default {
+  name: "App",
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
